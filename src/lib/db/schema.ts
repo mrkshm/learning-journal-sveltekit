96abf32fe6245d265cd0eaa8b4e3be1f1861722e
mrks <hey@mrks.me>
2024-01-27 00:43:37 +0100

    feat: display entries by week

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -3,9 +3,11 @@ import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 export const entries = sqliteTable('entries', {
 	id: integer('id').primaryKey({ autoIncrement: true }),
-	category: text('category').default('work'),
-	description: text('description'),
-	date: text('date').default(sql`CURRENT_TIMESTAMP`)
+	category: text('category').notNull().default('work'),
+	description: text('description').notNull(),
+	date: text('date')
+		.notNull()
+		.default(sql`CURRENT_TIMESTAMP`)
 });
 
 export type Entry = InferSelectModel<typeof entries>;
